04d12c74c1f1dbc973cc7d27b082b4bd528da7cd
Saswat <saswatsubhajyotimallick@gmail.com>
2020-09-14 11:06:05 +0530

    added detection ctpn

---
+++ detect/config.py
@@ -0,0 +1,28 @@
+import os
+
+base_dir = './images'
+img_dir = os.path.join(base_dir, 'VOC2007_text_detection/JPEGImages')
+xml_dir = os.path.join(base_dir, 'VOC2007_text_detection/Annotations')
+
+icdar17_mlt_img_dir = '/home/user/ole/ICDAR2017_MLT/train/'
+icdar17_mlt_gt_dir = '/home/user/ole/ICDAR2017_MLT/train_gt/'
+num_workers = 2
+pretrained_weights = 'checkpoints/base.pth.tar'
+
+train_txt_file = os.path.join(base_dir, r'VOC2007_text_detection/ImageSets/Main/train.txt')
+val_txt_file = os.path.join(base_dir, r'VOC2007_text_detection/ImageSets/Main/val.txt')
+
+
+anchor_scale = 16
+IOU_NEGATIVE = 0.3
+IOU_POSITIVE = 0.7
+IOU_SELECT = 0.7
+
+RPN_POSITIVE_NUM = 150
+RPN_TOTAL_NUM = 300
+
+IMAGE_MEAN = [123.68, 116.779, 103.939]
+
+
+checkpoints_dir = './checkpoints'
+outputs = r'./logs'
